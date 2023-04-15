63a10555c7211560bd504cd23d13561439fc6420
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-15 21:32:41 +0100

    Added onClick event

--- app/src/01-useState/error-example.jsx
+++ app/src/01-useState/error-example.jsx
@@ -1,5 +1,20 @@
 const ErrorExample = () => {
-    return <h2>useState error example</h2>;
-  };
-  
-  export default ErrorExample;
+  let count = 0;
+  const handleClick = () => {
+    count = count + 1;
+    console.log(count);
+    return count;
+  }
+
+  return (
+    <>
+      <h2>useState error example</h2>
+      <p>{count}</p>
+      <button onClick={handleClick} className="btn">
+        Increment
+      </button>
+    </>
+  );
+};
+
+export default ErrorExample;
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import UseStateBasics from './01-useState/useState-basics';
+import "./styles.css";
 
 function App() {
   return (
     <div className="container">
-     
+      <UseStateBasics />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
